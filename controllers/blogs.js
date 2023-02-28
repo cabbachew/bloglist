a3396eefd5c469355afc6f97b057dbd76efc5b93
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-27 23:44:04 -0800

    move route handlers into a dedicated module in controllers directory

---
+++ controllers/blogs.js
@@ -0,0 +1,64 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+// Get all blogs
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+// Create a new blog
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
+
+// Show a single blog
+// blogsRouter.get('/:id', (request, response, next) => {
+//   Blog
+//     .findById(request.params.id)
+//     .then(blog => {
+//       if (blog) {
+//         response.json(blog)
+//       } else {
+//         response.status(404).end()
+//       }
+//     })
+//     .catch(error => next(error))
+// })
+
+// Delete a blog
+// blogsRouter.delete('/:id', (request, response, next) => {
+//   Blog
+//     .findByIdAndRemove(request.params.id)
+//     .then(result => {
+//       response.status(204).end()
+//     })
+//     .catch(error => next(error))
+// })
+
+// Update a blog
+// blogsRouter.put('/:id', (request, response, next) => {
+//   const body = request.body
+//
+//   const blog = {
+//     title: body.title,
+//     ...
+//   }
+//
+//   Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+//     .then(updatedBlog => {
+//       response.json(updatedBlog)
+//     })
+//     .catch(error => next(error))
+// })
