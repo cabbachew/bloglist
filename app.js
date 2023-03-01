3e6a3705c59facb38792f8824b4f8c8bc051cd43
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-28 22:54:02 -0800

    extract printing to the console into a separate module with info and error functions

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-console.log('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
 app.use(cors())
