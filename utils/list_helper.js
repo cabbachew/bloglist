5285e50274d2159ca243727be3a45c0a1253a212
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-28 16:03:17 -0800

    implement function to sum likes for a blog list and write corresponding tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+// Receives an array of blog posts and returns the total likes
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
