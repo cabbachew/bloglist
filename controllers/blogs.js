ead76367e2da9ea0210938897dea9a5e6c801726
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-01 21:23:27 -0800

    refactor route handlers to fetch all blogs and create one using async/await syntax

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,41 +8,44 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 // Create a new blog
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  // const body = request.body
+  // const blog = new Blog({
+  //   title: body.title,
+  //   author: body.author,
+  //   url: body.url,
+  //   likes: body.likes,
+  // })
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+
+  // blog
+  //   .save()
+  //   .then(result => {
+  //     response.status(201).json(result)
+  //   })
+  //   .catch(error => next(error))
 })
 
 module.exports = blogsRouter
 
 // Show a single blog
-// blogsRouter.get('/:id', (request, response, next) => {
-//   Blog
-//     .findById(request.params.id)
-//     .then(blog => {
-//       if (blog) {
-//         response.json(blog)
-//       } else {
-//         response.status(404).end()
-//       }
-//     })
-//     .catch(error => next(error))
+// blogsRouter.get('/:id', async (request, response, next) => {
+//   const blog = await Blog.findById(request.params.id)
+//   if (blog) {
+//     response.json(blog)
+//   } else {
+//     response.status(404).end()
+//   } 
 // })
 
 // Delete a blog
-// blogsRouter.delete('/:id', (request, response, next) => {
-//   Blog
-//     .findByIdAndRemove(request.params.id)
-//     .then(result => {
-//       response.status(204).end()
-//     })
-//     .catch(error => next(error))
+// blogsRouter.delete('/:id', async (request, response, next) => {
+//   await Blog.findByIdAndRemove(request.params.id)
+//   response.status(204).end()
 // })
 
 // Update a blog
