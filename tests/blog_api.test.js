a2d98e3e36a7cda34bdb3b76dd21e70c873cf806
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-01 21:48:29 -0800

    write a test to verify that the id property is defined for blog posts

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -35,6 +35,11 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('the unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
 // test('a specific blog is within the returned blogs', async () => {
 //   const response = await api.get('/api/blogs')
 
