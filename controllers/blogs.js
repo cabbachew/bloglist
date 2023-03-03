19c76a7e572cfed48ddb9917cad0dbcd77d556ed
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-03 12:47:47 -0800

    modify handling of blog creation to reference and update user object

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 // Get all blogs
 blogsRouter.get('/', async (request, response) => {
@@ -9,17 +10,22 @@ blogsRouter.get('/', async (request, response) => {
 
 // Create a new blog
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body =request.body
 
-  // const body = request.body
-  // const blog = new Blog({
-  //   title: body.title,
-  //   author: body.author,
-  //   url: body.url,
-  //   likes: body.likes,
-  // })
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id
+  })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  
   response.status(201).json(savedBlog)
 
   // blog
