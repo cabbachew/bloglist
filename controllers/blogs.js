ea1ce8ece935889a40e7b96a53cea67a170f67aa
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-03 22:18:18 -0800

    verify post ownership and update user when deleting a blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,8 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+// const User = require('../models/user')
+// const jwt = require('jsonwebtoken')
+const { userExtractor } = require('../utils/middleware')
 
 // Get all blogs
 blogsRouter.get('/', async (request, response) => {
@@ -11,14 +12,9 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 // Create a new blog
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', userExtractor, async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  // decoded object contains the username and id fields
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog({
     title: body.title,
@@ -42,14 +38,6 @@ blogsRouter.post('/', async (request, response) => {
   //   .catch(error => next(error))
 })
 
-// Delete a blog
-blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
-
-module.exports = blogsRouter
-
 // Show a single blog
 blogsRouter.get('/:id', async (request, response) => {
   const blog = await Blog.findById(request.params.id)
@@ -71,3 +59,21 @@ blogsRouter.put('/:id', async (request, response) => {
     response.status(404).end()
   }
 })
+
+// Delete a blog
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  const user = request.user
+
+  const blogToDelete = await Blog.findById(request.params.id)
+  if (blogToDelete.user.toString() === user.id.toString()) {
+    await blogToDelete.remove()
+    // Also remove the blog from the user's blogs array
+    user.blogs = user.blogs.filter(b => b.id.toString() !== blogToDelete.id.toString())
+    await user.save()
+    response.status(204).end()
+  } else {
+    response.status(401).json({ error: 'user not authorized to delete this blog' })
+  }
+})
+
+module.exports = blogsRouter
