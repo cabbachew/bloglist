5541c6eaff4fa0a5835821e3c1ab566a782e9023
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-03 19:15:03 -0800

    modify fetching of all users to display blogs created by each user

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,8 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({}).populate('blogs', { url: 1, title: 1, author: 1 })
   response.json(users)
 })
 
@@ -15,7 +16,7 @@ usersRouter.post('/', async (request, response) => {
       error: 'password must be at least 3 characters long'
     })
   }
-  
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
