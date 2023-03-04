69842e1c152a2290dc8b7942b9acb2f7479dc1fc
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-03 22:22:50 -0800

    define test helper to return a user's token

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,5 +1,7 @@
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
 
 const initialBlogs = [
   {
@@ -59,9 +61,21 @@ const usersInDb = async () => {
   return users.map(user => user.toJSON())
 }
 
+const getToken = async (username) => {
+  const user = await User.findOne({ username: username })
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  return jwt.sign(userForToken, config.SECRET)
+}
+
 module.exports = {
   initialBlogs,
   nonExistingId,
   blogsInDb,
-  usersInDb
+  usersInDb,
+  getToken
 }
