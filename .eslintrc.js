a793db8df3e217f4972fd1e06e927b5845658c46
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-21 17:49:03 -0400

    configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,13 @@
 module.exports = {
-  'env': {
-    'node': true,
-    'commonjs': true,
-    'es2021': true,
-    'jest': true
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
   },
-  'extends': 'eslint:recommended',
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest'
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
   },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ]
-  }
-}
+};
